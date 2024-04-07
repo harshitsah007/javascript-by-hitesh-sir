25304d5c7eb4541367ac8b3c8c8730c93261dac9
harshitsah007 <166163419+harshitsah007@users.noreply.github.com>
2024-04-07 17:44:16 +0000

    full explaination of data types

--- .devcontainer/basic/datatypes.js
+++ .devcontainer/basic/datatypes.js
@@ -13,9 +13,43 @@
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **************************Operations*************************
 
 
+let value = 6;
+let negValue = -value;
+// console.log(negValue);
 
+// console.log(3 + 4 * 5 % 6);
 
+//  === strict check 
+
+// data types are mainly two types  1- primitiv , 2- non-primitiv
+
+/* 7 types : String, Number, Boolean, Null, undefined, Symbol,
+BigInt,
+
+const id = symbol('1233');
+const secondId = symbol('1233'); give uniquness
+
+In dynamically typed languages all type checks are performed in 
+a runtime, only when your program is executing. So this means you can
+ just assign anything you want to the variable and it will work.
+
+refrence(Non Premitive): Array, Object, Functions
+
+*/
+
+// const team =['MI','csk','GL','srh'];
+// const myObj={
+//     name: 'Himanshu',
+//     classs: '12th',
+//     school : hhhhsss,
+// }
+
+// const myFunction = function(){
+//     console.log("hello world")
+// }
