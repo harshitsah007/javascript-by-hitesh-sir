330a997c8e06606d422bb25e768be6f097a6ad94
harshitsah007 <166163419+harshitsah007@users.noreply.github.com>
2024-04-07 19:41:50 +0000

    some string function

---
+++ .devcontainer/basic/string.js
@@ -0,0 +1,17 @@
+const name = "harshit";
+const repoCount= 5;
+
+console.log(name + repoCount);
+
+console.log(`my official name is ${name} and my github repo count are ${repoCount}`);
+
+
+const newName = new String ('himanshu');
+// console.log(newName);
+// console.log(newName[0]);
+// console.log(newName.toUpperCase());
+// console.log(newName);
+
+const newString = newName.substring(0, 4);
+console.log(newString);
+console.log(newName);
