2270ee1f443d750f13b227df11bdeb96f9d20cc6
harshitsah007 <166163419+harshitsah007@users.noreply.github.com>
2024-04-08 21:20:46 +0000

    object in js

--- .devcontainer/02_basic/03_objects.js
+++ .devcontainer/02_basic/03_objects.js
@@ -30,6 +30,37 @@ myDetails.greetingTwo = function(){
     console.log(`hello my lovly friend, My name is ${this.Name}`)
 }
 
-console.log(myDetails.greeting());
-console.log(myDetails.greetingTwo());
-console.log(myDetails);
+// console.log(myDetails.greeting());
+// console.log(myDetails.greetingTwo());
+// console.log(myDetails);
+
+
+const user = {
+    email: "himanshu@chatgtp.com",
+    name:{
+        userName:{
+            fullName :{
+                firstName:"harshit",
+                lastName: "bhole"
+            }
+        }
+    }
+}
+
+console.log(user.name.userName.fullName.lastName);
+
+const obj1 = {1:"A",2:"B"}
+const obj2 = {3:"A",4:"B"}
+const obj3 = {5:"A",6:"B"}
+
+// const obj4= {obj1,obj2,obj3};
+// const obj4 = Object.assign({},obj1,obj2,obj3);
+const obj4 = {...obj1,...obj2,...obj3};
+
+// console.log(obj4);
+
+// console.log(Object.keys(myDetails));
+// console.log(Object.values(myDetails)); // value pair array
+// console.log(Object.entries(myDetails));//make all key value pair in array
+
+console.log(myDetails.hasOwnProperty('Name'));
