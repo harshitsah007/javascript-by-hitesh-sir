d6d39e36d04c43fb87b5257f887392134fbb109b
harshitsah007 <166163419+harshitsah007@users.noreply.github.com>
2024-04-09 18:32:06 +0000

    object and array in function

--- .devcontainer/03_basic/01_function.js
+++ .devcontainer/03_basic/01_function.js
@@ -36,5 +36,35 @@ function isLogedIn(username= "harsh"){
 
 }
 
-console.log(isLogedIn()); 
+// console.log(isLogedIn()); 
 
+// 
+
+function totalCartItem(val1,val2,...num1){
+    console.log(val1,val2);
+    return  num1;
+}
+
+// console.log(totalCartItem(200,400,500,600));
+const user = {
+    username: "himanshu",
+    price: 199,
+}
+
+function handleObject(objects){
+    console.log(`this is user ${objects.username} and the total price is ${objects.price}`);
+}
+
+// console.log(handleObject(user));
+handleObject({
+    username:"harshit",
+    price: 200,
+})
+
+const newArray = [20,40,30,48,90];
+
+function returnArray(getArray){
+    console.log(getArray[1]);
+}
+
+returnArray(newArray);
